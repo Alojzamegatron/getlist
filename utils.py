69c27de86abdc64a0097b37a3eb6ca52360bfba5
Alojzamegatron <BarbaraSchultz321@outlook.com>
2025-06-10 06:30:00 +0300

    Add add_task

---
+++ utils.py
@@ -0,0 +1,16 @@
+# utils.py
+
+def add_task(task):
+    with open('todo.txt', 'a') as f:
+        f.write(task + '\n')
+
+def list_tasks():
+    with open('todo.txt', 'r') as f:
+        return f.readlines()
+
+def remove_task(index):
+    tasks = list_tasks()
+    if 0 <= index < len(tasks):
+        tasks.pop(index)
+        with open('todo.txt', 'w') as f:
+            f.writelines(tasks)
